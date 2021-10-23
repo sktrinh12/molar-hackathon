1312516e4892db68069e615a68b1f18cd1b0fb90
José Manuel Nápoles Duarte <jnapoles@uach.mx>
2021-10-23 12:35:29 -0600

    path

--- app.py
+++ app.py
@@ -13,11 +13,11 @@ st.markdown('SegmentLabel: '+segment)
 slidename=st.sidebar.text_input('SlideName')
 st.markdown('SlideName: '+slidename)
 
-df1=df[['ROICoordinateX','ROICoordinateY','SlideName']]
+df1=df[['ROICoordinateX','ROICoordinateY','SlideName','pathology']]
 
 df1=df1[df1['SlideName']=='disease3']
 
 c = alt.Chart(df1).mark_circle().encode(
-    x='ROICoordinateX', y='ROICoordinateY',color='SlideName')
+    x='ROICoordinateX', y='ROICoordinateY',color='pathology')
 
 st.altair_chart(c, use_container_width=True)
