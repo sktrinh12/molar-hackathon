90f023718f574274143756af3d2316d86d1d4579
José Manuel Nápoles Duarte <jnapoles@uach.mx>
2021-10-23 10:56:05 -0600

    df

--- app.py
+++ app.py
@@ -1,3 +1,8 @@
 import streamlit as st
-
+import pandas as pd
 st.markdown('# Hackaton project')
+
+df=pd.read_csv('https://raw.githubusercontent.com/napoles-uach/Nanostring/main/Kidney_Sample_Annotations.csv')
+
+df.head()
+
